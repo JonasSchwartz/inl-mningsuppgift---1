208f4542808a51e19aeccf1768251500d1253e56
jonas <jonas_schwartz@hotmail.com>
2021-12-27 09:45:04 +0100

    first commit

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
 const form = document.getElementById('form')
 const firstName = document.getElementById('firstName')
 const lastName = document.getElementById('lastName')
-const eMail = document.getElementById('Email')
-const userName = Math.random()*1000
-console.log(userName)
+const email = document.getElementById('email')
 
-form.addEventListener('submit',(e) => {
+form.addEventListener('submit', e => {
     e.preventDefault();
-    checkinputs();
-})
 
-function checkinputs() {
-    const firstNameValue = firstName.value()
-    const lastNameValue = lastName.value()
-    const emailVallue = eMail.value()
-    
+    validateInputs();
+});
+const setError = (element,message) => {
+const inputControl = element.parentElement;
+const errorDisplay = inputControl.querySelector('.error');
 
-    if(firstNameValue === '') {
-        setErrorFor(userName,'Användarnamnet kan inte vara tomt')
-    } else {
-        setSuccessFor(userName);
-    }
+errorDisplay.innerText = message;
+inputControl.classList.add('error')
+inputControl.classList.remove('success')
 }
-function setErrorFor (input,message) {
-    const formControl = input.parentElemnt;
-    const small = formControl.querySelector('small')
-    small.innerText = message;
 
-    formControl.className = 'form-control error'
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error')
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success')
+    inputControl.classList.remove('error')
+}
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
+const validateInputs = () => {
+const firstNameValue = firstName.value.trim();
+const lastNameValue = lastName.value.trim();
+const emailValue = email.value.trim();
+}
+
+if(firstNameValue === '') {
+    setError(firstname,'Ange ett förnamn')
+
+} else {
+setSuccess(firstname)
+}
+
+if (emailValue === '') {
+    setError(email,'Ange en epost')
+}else if (!isValidEmail(emailValue)) {
+    setError(email,'Ange en giltig epost')
+} else {
+    setSuccess(email)
 }
