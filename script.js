4e06a0e20f894a1a3e8c88603921637970264188
jonas <jonas_schwartz@hotmail.com>
2021-12-27 12:19:07 +0100

    first commit

--- script.js
+++ script.js
@@ -2,53 +2,57 @@ const form = document.getElementById('form')
 const firstName = document.getElementById('firstName')
 const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
+const userArray = ['']
+const userNumber = Math.floor((Math.random()*1000)+1)
+console.log(userNumber)
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit',(e) => {
     e.preventDefault();
 
-    validateInputs();
-});
-const setError = (element,message) => {
-const inputControl = element.parentElement;
-const errorDisplay = inputControl.querySelector('.error');
+    checkInputs();
 
-errorDisplay.innerText = message;
-inputControl.classList.add('error')
-inputControl.classList.remove('success')
+})
+
+function checkInputs() {
+    const firstNameValue = firstName.value.trim()
+    const lastNameValue = lastName.value.trim()
+    const emailValue= email.value.trim()
+
+    if (firstName.value === '') {
+        
+        setErrorFor(firstName,'kan inte vara blankt')
+    }else {
+        
+        setSuccessFor(firstName);
+    }
+    if(lastNameValue === '') {
+        setErrorFor(lastName,'kan inte vara blankt')
+    } else {
+        setSuccessFor(lastName)
+    }
+    if(emailValue ==='') {
+        setErrorFor(email, 'Ange en epost')
+    } else if (!validEmail(emailValue))
+    setErrorFor(email, 'Ange en giltig epost')
+
+    else {
+        setSuccessFor(email)
+    }
+} 
+function setErrorFor(input,message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
 }
 
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error')
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success')
-    inputControl.classList.remove('error')
+function setSuccessFor (input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+function validEmail (email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
-
-
-const validateInputs = () => {
-const firstNameValue = firstName.value.trim();
-const lastNameValue = lastName.value.trim();
-const emailValue = email.value.trim();
-}
-
-if(firstNameValue === '') {
-    setError(firstname,'Ange ett förnamn')
-
-} else {
-setSuccess(firstname)
-}
-
-if (emailValue === '') {
-    setError(email,'Ange en epost')
-}else if (!isValidEmail(emailValue)) {
-    setError(email,'Ange en giltig epost')
-} else {
-    setSuccess(email)
-}
